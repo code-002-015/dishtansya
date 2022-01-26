af48fca821332a93d2d012e064fa36b7f3ff4474
ryanolasco.jdk <ryanolasco@gmail.com>
2022-01-26 12:54:11 +0800

    email jobs

---
+++ app/Jobs/SendEmailRegistrationJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use App\Mail\SendEmailRegistration;
+use Mail;
+
+class SendEmailRegistrationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($send_mail)
+    {
+        $this->send_mail = $send_mail;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $email = new SendEmailRegistration();        
+        Mail::to($this->send_mail)->send($email);
+    }
+}
---
+++ resources/views/mail/mail-registration.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email Registration</title>
+</head>
+<body>
+   
+  
+<p>Hi,</p>
+<p>You’re receiving this email because you recently created a new Dishtansya account or added a new email address. All you need to do is verify your email address to activate your Dishtansya account.</p>  
+<strong>Thanks & Regards.</strong>
+
+</body>
+</html>
